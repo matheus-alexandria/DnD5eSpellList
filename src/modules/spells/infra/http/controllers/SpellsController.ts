28a8ec78bd8b983a863a384ff89631c64a232005
Matheus Alexandria <maalexandria@hotmail.com>
2020-07-15 13:05:48 -0300

    Filter by level and type of spell

--- src/modules/spells/infra/http/controllers/SpellsController.ts
+++ src/modules/spells/infra/http/controllers/SpellsController.ts
@@ -3,13 +3,21 @@ import { container } from 'tsyringe';
 
 import ListAllSpellsService from '@modules/spells/services/ListAllSpellsService';
 
+interface IRequest {
+  level: string | undefined;
+  type: string | undefined;
+}
+
 export default class SpellsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { level, type } = request.query;
 
     const listAllSpells = container.resolve(ListAllSpellsService);
 
-    const spells = await listAllSpells.execute({ level: Number(level), type });
+    const spells = await listAllSpells.execute({
+      level: Number(level),
+      type: String(type),
+    });
 
     return response.json(spells);
   }
